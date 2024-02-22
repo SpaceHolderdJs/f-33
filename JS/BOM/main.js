e649c3bae204f9adc808b59d9e51346b5277cd2b
Igor <sergienko339@gmail.com>
2024-02-22 22:04:48 +0200

    bom l2

--- JS/BOM/main.js
+++ JS/BOM/main.js
@@ -30,6 +30,28 @@ const normalizeTimeValue = (timeValue) => {
   return timeValue.toString().length > 1 ? `${timeValue}` : `0${timeValue}`;
 };
 
+const getTime = (seconds) => {
+  const date = new Date();
+
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+
+  date.setMilliseconds(seconds);
+
+  const dateHours = date.getHours();
+  const dateMinutes = date.getMinutes();
+  const dateSeconds = date.getSeconds();
+  const dateMilliseconds = String(date.getMilliseconds()).slice(0, 2);
+
+  return `${normalizeTimeValue(dateHours)}:${normalizeTimeValue(
+    dateMinutes
+  )}:${normalizeTimeValue(dateSeconds)}:${normalizeTimeValue(
+    dateMilliseconds
+  )}`;
+};
+
 let timer = undefined;
 let secondsValue = 0;
 
@@ -39,14 +61,16 @@ const buttons = {
   start: document.querySelector("#start-btn"),
   stop: document.querySelector("#stop-btn"),
   reset: document.querySelector("#reset-btn"),
+  decrease: document.querySelector("#decrease-10"),
+  increase: document.querySelector("#increase-10"),
 };
 
 buttons.start.onclick = () => {
   if (!timer) {
     timer = setInterval(() => {
-      secondsValue = secondsValue + 1;
-      seconds.textContent = normalizeTimeValue(secondsValue);
-    }, 1000);
+      secondsValue = secondsValue + 100;
+      seconds.textContent = getTime(secondsValue);
+    }, 100);
   }
 };
 
@@ -57,9 +81,77 @@ buttons.stop.onclick = () => {
 
 buttons.reset.onclick = () => {
   secondsValue = 0;
-  seconds.textContent = "00";
+  seconds.textContent = "00:00:00:00";
 };
 
-// const normalizeTimeValue1 = (timeValue) => {
-//   return timeValue < 10 ? `0${timeValue}` : `${timeValue}`;
-// };
+buttons.decrease.onclick = () => {
+  secondsValue = secondsValue - 10000 >= 0 ? secondsValue - 10000 : 0;
+  seconds.textContent = getTime(secondsValue);
+};
+
+buttons.increase.onclick = () => {
+  secondsValue += 10000;
+  seconds.textContent = getTime(secondsValue);
+};
+
+const reloadButton = document.querySelector("#reload");
+
+reloadButton.onclick = () => {
+  window.location.reload();
+};
+
+console.log(window.location);
+console.log(window.navigator);
+
+const copyButton = document.querySelector("#copy");
+const pasteInput = document.querySelector("#paste");
+
+copyButton.onclick = async () => {
+  await window.navigator.clipboard.writeText("Copy");
+  const text = await window.navigator.clipboard.readText();
+  pasteInput.value = text;
+};
+
+pasteInput.oninput = (event) => {
+  window.navigator.clipboard.writeText(event.target.value);
+};
+
+window.navigator.geolocation.getCurrentPosition(
+  (position) => {
+    console.log(position, "positon");
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+window.navigator.geolocation.watchPosition(
+  (postion) => {
+    console.log(postion, "position");
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+// not works on desktop
+// console.log(
+//   window.navigator.bluetooth.getAvailability().then((r) => {
+//     console.log(r, "!!!");
+//     window.navigator.bluetooth.requestDevice();
+//   }),
+//   "bluetooth"
+// );
+
+console.log(window.navigator.language, "language");
+console.log(window.navigator.languages, "langs");
+
+window.navigator.languages
+  .filter((langName) => langName.length === 2)
+  .forEach((langName) => {
+    const button = document.createElement("button");
+    button.textContent = langName.toUpperCase();
+
+    button.onclick = () => console.log(langName);
+    document.body.appendChild(button);
+  });
