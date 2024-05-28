5af45fa740e77a9e98e32ff5362f31d4f622e7a7
Igor <sergienko339@gmail.com>
2024-05-28 22:26:57 +0300

    angular hooks

---
+++ Angular/users-dashboard/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { UsersModule } from './users/users.module';
+import { routes } from './app.routes';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, UsersModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  headerLinks = routes;
+}
